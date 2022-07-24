bae36405ff1aacced27c8486837bf65a75bf334b
PG12138 <tmd@tencent>
2022-07-24 22:25:55 +0800

    tcp

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,32 @@
 use std::collections::{VecDeque, HashMap, hash_map};
 use std::error::Error;
-use std::sync::{Arc, Mutex};
+use std::io::{Read, Write};
+use std::net::Ipv4Addr;
+use std::sync::{Arc, Mutex, Condvar};
 use std::{thread, io};
 
 
 use tun_tap;
 use smoltcp::wire::{self, PrettyPrinter};
-use smoltcp::wire::Ipv4Address;
 
-
+mod tcp;
+const SENDQUEUE_SIZE: usize = 1024;
 
 
 
 type Port = u16;
+type Connection = tcp::Connection;
+
 #[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
 struct Quad {
-    src: (Ipv4Address, Port),
-    dst: (Ipv4Address, Port),
-}
-
-struct Connection {
-    
+    src: (Ipv4Addr, Port),
+    dst: (Ipv4Addr, Port),
 }
 
 struct ConnectionPool_inner {
-    connection: HashMap<Quad, Arc<Mutex<Connection>>>,
-    listener: HashMap<Port, Arc<Mutex<Vec<Quad>>>>
+    connection: Mutex<HashMap<Quad, Arc<(Mutex<Connection>,Condvar)>>>,
+    listener: Mutex<HashMap<Port, VecDeque<Quad>>>,
+    pending_var: Condvar,
 }
 
 impl Default for ConnectionPool_inner {
@@ -37,43 +38,45 @@ impl Default for ConnectionPool_inner {
 impl ConnectionPool_inner {
     fn new() -> Self {
         Self {
-            connection: HashMap::default(),
-            listener: HashMap::default()
+            connection: Mutex::default(),
+            listener: Mutex::default(),
+            pending_var: Condvar::default()
         }
     }
-    fn add_connection(&mut self, quad:Quad, cn:Connection) -> Arc<Mutex<Connection>>{
-        let cn = Arc::new(Mutex::new(cn));
-        self.connection.insert(quad, cn.clone());
+    fn add_connection(&self, quad:Quad, cn:Connection) -> Arc<(Mutex<Connection>, Condvar)>{
+        let cn = Arc::new((Mutex::new(cn), Condvar::default()));
+        self.connection.lock().unwrap().insert(quad, cn.clone());
         cn
     }
-    fn get() -> io::Result<Connection> {
-        unimplemented!()
+    fn get(&self, q: &Quad) -> Option<Arc<(Mutex<Connection>, Condvar)>> {
+        self.connection.lock().unwrap().get(q).map(|c| {
+            c.clone()
+        })
     }
-    fn remove(&mut self, quad:Quad) -> Option<Arc<Mutex<Connection>>> {
-        self.connection.remove(&quad)
+    fn remove(&self, quad:Quad) -> Option<Arc<(Mutex<Connection>, Condvar)>> {
+        self.connection.lock().unwrap().remove(&quad)
     }
-    fn bind(&mut self, port: Port) -> Option<Arc<Mutex<Vec<Quad>>>> {
-        if self.listener.contains_key(&port) {
-            None
+    fn bind(&self, port: Port) -> io::Result<()>{
+        if self.listener.lock().unwrap().contains_key(&port) {
+            Err(io::Error::new(
+                io::ErrorKind::AddrInUse,
+                "port already bound",
+            ))
         } else {
-            let listener_queue: Arc<Mutex<Vec<Quad>>> = Arc::default();
-            self.listener.insert(port, listener_queue.clone());
-            Some(listener_queue)
+            self.listener.lock().unwrap().insert(port, VecDeque::default());
+            Ok(())
         }
     }
 }
 
-type ConnectionPool = Arc<Mutex<ConnectionPool_inner>>;
+type ConnectionPool = Arc<ConnectionPool_inner>;
 
-
-
-struct Interface {
+pub struct Interface {
     connection_pool: ConnectionPool,
-    tuntap_interface: Arc<tun_tap::Iface>
 }
 
 // Establish a connection Remove a connection Send messge to connection.
-fn packet_loop(nic: Arc<tun_tap::Iface>, connection_pool: ConnectionPool) -> io::Result<()>{
+fn packet_loop( mut nic:tun_tap::Iface, connection_pool: ConnectionPool) -> io::Result<()>{
     let mut buf = [0u8; 1504];
     let mut try_recv = move || -> Result<(), Box<dyn Error>> {
         let nbytes = nic.recv(&mut buf[..])?;
@@ -81,17 +84,32 @@ fn packet_loop(nic: Arc<tun_tap::Iface>, connection_pool: ConnectionPool) -> io:
             "{}",
             PrettyPrinter::<wire::Ipv4Packet<&[u8]>>::new("", &&buf[..nbytes])
             );
-        let ipv4header = wire::Ipv4Packet::new_checked(&buf[..nbytes])?;
-        let tcpheader = wire::TcpPacket::new_checked(ipv4header.payload())?;
+        let iph = etherparse::Ipv4HeaderSlice::from_slice(&buf[..nbytes])?;
+        let tcph = etherparse::TcpHeaderSlice::from_slice(&buf[iph.slice().len()..nbytes])?;
         let q = Quad{
-            src: (ipv4header.src_addr(), tcpheader.src_port()),
-            dst: (ipv4header.dst_addr(), tcpheader.dst_port())
+            src: (iph.source_addr(), tcph.source_port()),
+            dst: (iph.destination_addr(), tcph.destination_port())
         };
-        let mut cnp = connection_pool.lock().unwrap();
-        let cn = &mut *cnp;
-        match cn.connection.entry(q) {
-            hash_map::Entry::Occupied(c) => {},
-            hash_map::Entry::Vacant(e) => {}
+        let cn = &*connection_pool;
+        let datai = iph.slice().len() + tcph.slice().len();
+        match cn.connection.lock().unwrap().entry(q) {
+            hash_map::Entry::Occupied(mut c) => {
+                println!("recv message");
+                let (cn, var) = &**(c.get_mut());
+                let a = cn.lock().unwrap().on_packet(&mut nic, iph, tcph, &buf[datai..nbytes])?;
+                var.notify_all();
+            },
+            hash_map::Entry::Vacant(e) => {
+                println!("accept");
+                if let Some(listener) = cn.listener.lock().unwrap().get_mut(&tcph.destination_port()) {
+                    if let Some(c) = tcp::Connection::accept( &mut nic, iph, tcph, &buf[datai..nbytes],)? {
+                        let c = Arc::new((Mutex::new(c), Condvar::default()));
+                        e.insert(c);
+                        listener.push_back(q);
+                        cn.pending_var.notify_all();
+                    }
+                }
+            }
         }
         Ok(())
     };
@@ -104,34 +122,121 @@ fn packet_loop(nic: Arc<tun_tap::Iface>, connection_pool: ConnectionPool) -> io:
 }
 
 impl Interface {
-    fn new() -> io::Result<Self> {
+    pub fn new() -> io::Result<Self> {
         let nic = tun_tap::Iface::without_packet_info("tun0", tun_tap::Mode::Tun)?;
-        let nic = Arc::new(nic);
-        let nic_temp = nic.clone();
         let con_pool: ConnectionPool = Arc::default();
         let temp = con_pool.clone();
-        thread::spawn(move || packet_loop( nic_temp, temp));
+        thread::spawn(move || packet_loop( nic, temp));
         Ok( Self {
             connection_pool: con_pool,
-            tuntap_interface: nic.clone()
         })
     }
-    fn bind() -> io::Result<TcpListener> {
-        unimplemented!()
+    pub fn bind(&mut self, port: Port) -> io::Result<TcpListener> {
+        self.connection_pool.bind(port).map(|_| {
+            TcpListener{
+                port,
+                con_pool: self.connection_pool.clone()
+            }
+        })
     }
-    fn connect() -> io::Result<TcpStream> {
+    pub fn connect() -> io::Result<TcpStream> {
         unimplemented!()
     }
 }
 
-struct TcpListener {
+pub struct TcpListener {
     port: Port,
     con_pool: ConnectionPool
 }
 
-struct TcpStream {
+impl TcpListener {
+    pub fn accept(&mut self) -> io::Result<TcpStream> {
+        loop {
+            println!("test");
+            let mut listener = self.con_pool.listener.lock().unwrap();
+            if let Some(quad) = listener.get_mut(&self.port).unwrap().pop_front() {
+                return Ok(TcpStream{
+                    quad,
+                    connection: self.con_pool.connection.lock().unwrap().get_mut(&quad).unwrap().clone()
+                })
+            }
+            listener = self.con_pool.pending_var.wait(listener).unwrap();
+        }
+    }
+}
+
+pub struct TcpStream {
     quad: Quad,
-    connection: Connection
+    connection: Arc<(Mutex<Connection>, Condvar)>
+}
+
+
+impl Read for TcpStream {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        loop {
+            let (c, var) = &*self.connection;
+            let mut c = c.lock().unwrap();
+
+            if c.is_rcv_closed() && c.incoming.is_empty() {
+                // no more data to read, and no need to block, because there won't be any more
+                return Ok(0);
+            }
+
+            if !c.incoming.is_empty() {
+                let mut nread = 0;
+                let (head, tail) = c.incoming.as_slices();
+                let hread = std::cmp::min(buf.len(), head.len());
+                buf[..hread].copy_from_slice(&head[..hread]);
+                nread += hread;
+                let tread = std::cmp::min(buf.len() - nread, tail.len());
+                buf[hread..(hread + tread)].copy_from_slice(&tail[..tread]);
+                nread += tread;
+                drop(c.incoming.drain(..nread));
+                return Ok(nread);
+            }
+            c = var.wait(c).unwrap();
+        }
+    }
+}
+
+impl Write for TcpStream {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let c = &mut self.connection.0.lock().unwrap();
+
+        if c.unacked.len() >= SENDQUEUE_SIZE {
+            // TODO: block
+            return Err(io::Error::new(
+                    io::ErrorKind::WouldBlock,
+                    "too many bytes buffered",
+                    ));
+        }
+
+        let nwrite = std::cmp::min(buf.len(), SENDQUEUE_SIZE - c.unacked.len());
+        c.unacked.extend(buf[..nwrite].iter());
+
+        Ok(nwrite)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        let c = &mut self.connection.0.lock().unwrap();
+
+        if c.unacked.is_empty() {
+            Ok(())
+        } else {
+            // TODO: block
+            Err(io::Error::new(
+                    io::ErrorKind::WouldBlock,
+                    "too many bytes buffered",
+                    ))
+        }
+    }
+}
+
+impl TcpStream {
+    pub fn shutdown(&self, how: std::net::Shutdown) -> io::Result<()> {
+        let c = &mut self.connection.0.lock().unwrap();
+        c.close()
+    }
 }
 
 #[cfg(test)]
@@ -142,22 +247,7 @@ mod test {
     // #[test]
     fn test_packet_loop() {
         let nic = tun_tap::Iface::without_packet_info("tap0", tun_tap::Mode::Tap).expect("Failed Creat nic");
-        let nic = Arc::new(nic);
         let con_pool: ConnectionPool = Arc::default();
         packet_loop(nic, con_pool);
     } 
-    #[test]
-    fn test_interface() {
-        let ih = Interface::new().unwrap();
-        let  arp_packet: &[u8] = &[255, 255, 255, 255, 255, 255, 2, 0, 0, 0, 0, 2, 8, 6, 0, 1, 8, 0, 6, 4, 0, 1, 2, 0, 0, 0, 0, 2, 192, 168, 69, 1, 255, 255, 255, 255, 255, 255, 192, 168, 69, 100];
-        let syn_packet: &[u8] = &[138, 244, 12, 41, 208, 162, 2, 0, 0, 0, 0, 2, 8, 0, 69, 0, 0, 52, 0, 0, 64, 0, 64, 6, 221, 112, 192, 168, 69, 1, 104, 196, 238, 229, 244, 55, 0, 80, 97, 48, 190, 195, 0, 0, 0, 0, 128, 2, 4, 0, 253, 71, 0, 0, 2, 4, 5, 180, 3, 3, 0, 4, 2, 0, 0, 0];
-        let ack_packet: &[u8] = &[138, 244, 12, 41, 208, 162, 2, 0, 0, 0, 0, 2, 8, 0, 69, 0, 0, 40, 0, 0, 64, 0, 64, 6, 221, 124, 192, 168, 69, 1, 104, 196, 238, 229, 244, 55, 0, 80, 97, 48, 190, 196, 220, 230, 242, 180, 80, 16, 4, 0, 106, 104, 0, 0];
-        println!("interface send");
-        ih.tuntap_interface.send(&arp_packet[..]);
-        thread::sleep(Duration::from_secs(10));
-        ih.tuntap_interface.send(&syn_packet[..]);
-        thread::sleep(Duration::from_secs(10));
-        ih.tuntap_interface.send(&ack_packet[..]);
-        thread::sleep(Duration::from_secs(10));
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,27 @@
+use std::io::prelude::*;
+use std::{io, thread};
 
-
-
-
-
-
-
-
-use core::time;
-use std::io::Error;
-
-use std::sync::{Arc, Mutex};
-use std::thread;
-
-use smoltcp::wire::{PrettyPrinter, EthernetFrame};
-
-use std::os::unix::io::AsRawFd;
-use tokio::io::{AsyncReadExt, ReadHalf, WriteHalf};
-use tokio_tun::result::Result;
-use tokio_tun::Tun;
-
-
-
-
-
-const ARP: &[u8] = &[255, 255, 255, 255, 255, 255, 2, 0, 0, 0, 0, 2, 8, 6, 0, 1, 8, 0, 6, 4, 0, 1, 2, 0, 0, 0, 0, 2, 192, 168, 69, 1, 255, 255, 255, 255, 255, 255, 192, 168, 69, 100];
-const SYN: &[u8] = &[138, 244, 12, 41, 208, 162, 2, 0, 0, 0, 0, 2, 8, 0, 69, 0, 0, 52, 0, 0, 64, 0, 64, 6, 221, 112, 192, 168, 69, 1, 104, 196, 238, 229, 244, 55, 0, 80, 97, 48, 190, 195, 0, 0, 0, 0, 128, 2, 4, 0, 253, 71, 0, 0, 2, 4, 5, 180, 3, 3, 0, 4, 2, 0, 0, 0];
-const STN2: &[u8] = &[138, 244, 12, 41, 208, 162, 2, 0, 0, 0, 0, 2, 8, 0, 69, 0, 0, 40, 0, 0, 64, 0, 64, 6, 221, 124, 192, 168, 69, 1, 104, 196, 238, 229, 244, 55, 0, 80, 97, 48, 190, 196, 220, 230, 242, 180, 80, 16, 4, 0, 106, 104, 0, 0];
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let tun = tokio_tun::TunBuilder::new()
-        .name("tap0")
-        .tap(true)
-        .packet_info(false)
-        .up()
-        .try_build()?;
-
-    println!("tun created, name: {}, fd: {}", tun.name(), tun.as_raw_fd());
-    let (mut reader, mut _writer) = tokio::io::split(tun);
-    listener(&mut reader).await;
-    println!("a");
-    listener(&mut reader).await;
-    println!("b");
-
-    let mut buf = [0u8; 1024];
-    loop {
-        let n = reader.read(&mut buf).await?;
-        println!("reading {} bytes: {:?}", n, &buf[..n]);
+fn main() -> io::Result<()> {
+    let mut i = TCPstack::Interface::new()?;
+    eprintln!("created interface");
+    let mut listener = i.bind(60)?;
+    while let Ok(mut stream) = listener.accept() {
+        eprintln!("got connection!");
+        thread::spawn(move || {
+            stream.write(b"hello from rust-tcp!\n").unwrap();
+            stream.shutdown(std::net::Shutdown::Write).unwrap();
+            loop {
+                let mut buf = [0; 512];
+                let n = stream.read(&mut buf[..]).unwrap();
+                eprintln!("read {}b of data", n);
+                if n == 0 {
+                    eprintln!("no more data!");
+                    break;
+                } else {
+                    println!("{}", std::str::from_utf8(&buf[..n]).unwrap());
+                }
+            }
+        });
     }
+    Ok(())
 }
-type Reader = Arc<Mutex<ReadHalf<Tun>>>;
-type Writer = Arc<Mutex<WriteHalf<Tun>>>;
-
-async fn listener(reader: &mut ReadHalf<Tun>){
-    println!("hello");
-    println!("hello");
-}
-
-fn notmain() {
-    let iface = tun_tap::Iface::without_packet_info("tap0", tun_tap::Mode::Tap).expect("Failed Creat tap interface!");
-    let iface = Arc::new(iface);
-    let liface = Arc::clone(&iface);
-    let wiface = Arc::clone(&iface);
-    let listener = thread::spawn(move || {
-        loop {
-            let mut rs = [0;1054];
-            let nbytes = liface.recv(&mut rs[..]).unwrap();
-            println!(
-                "{}",
-                PrettyPrinter::<EthernetFrame<&[u8]>>::new("", &&rs[..nbytes])
-                );
-        }
-    });
-    let writer = thread::spawn(move || {
-        // thread::sleep(Duration::from_secs(1));
-        let packet = ARP;
-        wiface.send(packet).unwrap();
-        // thread::sleep(Duration::from_secs(1));
-        let packet = SYN;
-        wiface.send(packet).unwrap();
-        let packet = STN2;
-        wiface.send(packet).unwrap();
-    });
-    listener.join().unwrap();
-    writer.join().unwrap();
-    println!("Done");
-}
-
